27101b4945dcecd34eb65e0062915a7d0f0e50f9
JoaquinRouge <Joarouge@gmail.com>
2023-08-31 16:48:00 -0300

    Modificaciones de color a las respuestas

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let parrafo = document.getElementById("parrafo")
-
-
+let boton1 = document.getElementById("incorrect1")
+let boton2 = document.getElementById("incorrect2")
+let boton3 = document.getElementById("incorrect3")
+let boton4 = document.getElementById("correct")
 
 function Correct() {
     parrafo.textContent = "✔️✔️✔️✔️"
@@ -8,4 +10,40 @@ function Correct() {
 
 function Incorrect() {
     parrafo.textContent = "❌❌❌❌"
+}
+
+boton1.addEventListener("click", color)
+
+function color() {
+    boton1.style.backgroundColor = "Red"
+    boton2.style.backgroundColor = "White"
+    boton3.style.backgroundColor = "White"
+    boton4.style.backgroundColor = "White"
+}
+
+boton2.addEventListener("click", color2)
+
+function color2() {
+    boton2.style.backgroundColor = "Red"
+    boton1.style.backgroundColor = "White"
+    boton3.style.backgroundColor = "White"
+    boton4.style.backgroundColor = "White"
+}
+
+boton3.addEventListener("click", color3)
+
+function color3() {
+    boton3.style.backgroundColor = "Red"
+    boton1.style.backgroundColor = "White"
+    boton2.style.backgroundColor = "White"
+    boton4.style.backgroundColor = "White"
+}
+
+boton4.addEventListener("click", color4)
+
+function color4() {
+    boton4.style.backgroundColor = "Green"
+    boton3.style.backgroundColor = "White"
+    boton1.style.backgroundColor = "White"
+    boton2.style.backgroundColor = "White"
 }
